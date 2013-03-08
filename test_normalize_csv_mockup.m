21f8629c3864ec2800c892f856f63f57ff15eb82
jessada <jessada@kth>
2013-03-08 15:33:29 +0100

    remove comment

--- test_normalize_csv_mockup.m
+++ test_normalize_csv_mockup.m
@@ -1,4 +1,4 @@
-function test_normalize_csv_mockup
+function [result] = test_normalize_csv_mockup
 
 size_x = 3;
 size_y = 4;
@@ -9,20 +9,16 @@ for i=1:size_x
     for j=1:size_y
         for k=1:size_z
             count = count + 1;
-            data  = [data; -0.1*i, -0.2*j, -0.3*k, count];
+            if mod(count,5) == 1
+                data  = [data; -0.1*i, -0.2*j, -0.3*k, 0];
+            else
+                data  = [data; -0.1*i, -0.2*j, -0.3*k, count];
         end;
     end;
 end;
-%disp(data)
-%data=csvread(csv_file);
-%disp(size(data))
-%disp(data(1:5,:))
-%disp(data([1,3,5],[1,2,4]))
 
 datapoint_2d = data(data(:,3) == -0.6, [1,2,4]);
-%datapoint_2d = data(:,[1,2,4]);
-%disp(datapoint_2d)
-disp(normalize_csv(datapoint_2d))
+result = normalize_csv(datapoint_2d);
 
 end
 
